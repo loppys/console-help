67418b05509d16b75bc7a26cd411bde5f4a931b9
loppys <foxgame1211@gmail.com>
2024-12-28 19:14:48 +0300

    init

---
+++ src/ConsoleLogger.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Vengine\Libraries\Console;
+
+class ConsoleLogger
+{
+    public static function mixedMessage(string $message, array $dataArray = []): void
+    {
+        $time = date('d.m.Y H:i:s');
+
+        if (!empty($dataArray)) {
+            $dataArray = json_encode($dataArray);
+            $message .= " :: {$dataArray}";
+        }
+
+        print "[{$time}] {$message}" . PHP_EOL;
+    }
+
+    public static function showMessage(string|array $message = ''): void
+    {
+        if (is_array($message)) {
+            $message = json_encode($message);
+        }
+
+        $time = date('d.m.Y H:i:s');
+
+        print "[{$time}] {$message}" . PHP_EOL;
+    }
+}
